2c0b9cc7fc9ed32d0e3b1c868109f9464d1102f5
akmot9 <95435335+Akmot9@users.noreply.github.com>
2023-05-08 22:46:05 +0200

    button-text area

---
+++ site/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <text-button />
+  </div>
+</template>
+
+<script>
+import TextButton from "./components/TextButton.vue";
+
+export default {
+  name: "App",
+  components: {
+    TextButton
+  }
+};
+</script>
---
+++ site/src/components/TextButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <textarea v-model="text" placeholder="Enter your text here"></textarea>
+      <button @click="onClick">Submit</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        text: ""
+      };
+    },
+    methods: {
+      onClick() {
+        // Handle button click event
+        console.log("Button clicked!");
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  textarea {
+    width: 100%;
+    height: 100px;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+  }
+  button {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  </style>
+  
